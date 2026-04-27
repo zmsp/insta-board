618904c512f5413307fe6a5c88d69a4d41c6787a
zmsp <zmsp@users.noreply.github.com>
2026-04-27 19:25:59 +0000

    Deploy from zmsp/insta_board_code@1d2013f821a41e4801df7546cbdcb088a739b60b

--- main.dart.js
+++ main.dart.js
@@ -33823,7 +33823,7 @@ $S:2}
 A.aL4.prototype={
 $1(a){var s=A.e7().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/59aa584fdf100e6c78c785d8a5b565d1de4b48ab/":s)+a},
 $S:45}
 A.SM.prototype={
 gB(a){var s=this.a
